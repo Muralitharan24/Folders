eb9f5d8846f3d73647c5669feacd01b256f96d98
Muralitharan24 <84373616+Muralitharan24@users.noreply.github.com>
2022-08-17 11:07:28 +0530

    Update script.js
    new commit

--- cart/Js/script.js
+++ cart/Js/script.js
@@ -1,48 +1,46 @@
 var leftbody = document.querySelector(".leftdv-body");
-var card = document.querySelectorAll(".my-card");
 var contentEl = document.querySelector(".content-dv");
-var count = document.querySelector(".countvalue")
+var count = document.querySelector(".countvalue");
 var cardArr = [{
       name : "Bedside Tables",
-      amount : "4,999",
+      amount : "4999",
       buttonTxt : "Add to cart"
 },
 {
     name : "Coffee  Tables",
-    amount : "4,999",
+    amount : "4999",
     buttonTxt : "Add to cart"
 },
 {
     name : "Rocking Chairs",
-    amount : "2,000",
+    amount : "2000",
     buttonTxt : "Add to cart"
 },
 {
     name : "Velvet Sofa",
-    amount : "15,000",
+    amount : "15000",
     buttonTxt : "Add to cart"
 },
 {
     name : "Taro Bed Side Table",
-    amount : "3,100",
+    amount : "3100",
     buttonTxt : "Add to cart"
 },
 {
     name : "Sofa",
-    amount : "10,000",
+    amount : "10000",
     buttonTxt : "Add to cart"
 },
 {
     name : "Bedside Tables",
-    amount : "4,999",
+    amount : "4999",
     buttonTxt : "Add to cart"
 },
 {
     name : "Rocking Chairs",
-    amount : "2,000",
+    amount : "2000",
     buttonTxt : "Add to cart"
 }]
-
 
 for(i = 0;i < cardArr.length;i++){
     var card = document.createElement("div");
@@ -62,14 +60,29 @@ for(i = 0;i < cardArr.length;i++){
     amountdv.innerHTML = cardArr[i].amount;
     button.innerHTML = cardArr[i].buttonTxt
     button.addEventListener("click",function(event){
+        var dvcontainer = document.createElement("div");
+        dvcontainer.className = "content-storer";
          var dv1 = document.createElement("div");
          dv1.className = "content-text1"
          var dv2 = document.createElement("div");
          dv2.className = "content-text2"
          dv1.innerHTML = event.target.previousElementSibling.firstChild.innerHTML;
          dv2.innerHTML = event.target.previousElementSibling.lastChild.innerHTML;
-         contentEl.append(dv1,dv2)
-        count.innerHTML = event.target.previousElementSibling.lastChild.innerHTML
+         contentEl.append(dv1,dv2);
+         var totalcount = 
+        count.innerHTML = totalcount
     })
+    function demo(){
+        var cardEl = document.querySelectorAll(".my-card");
+        var myiput = document.getElementById("iput").value.toLowerCase();
+         for(j = 0;j < cardEl.length;j++){
+             if(cardEl[j].firstChild.firstChild.innerHTML.toLowerCase().indexOf(myiput) != -1){
+                 cardEl[j].style.display = ""
+             }else{
+                 cardEl[j].style.display = "none"
+             } 
+         } 
+    }
 }
+
 
