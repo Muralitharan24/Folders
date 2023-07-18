caf77796965730061b508333b89bba88e3b797f3
Muralitharan24 <84373616+Muralitharan24@users.noreply.github.com>
2023-07-18 18:15:03 +0530

    Add files via upload
    new commit

--- formElements/js/script.js
+++ formElements/js/script.js
@@ -1,25 +1,45 @@
-var selectbtn = document.querySelectorAll(".option-focus");
+var body = document.querySelector("body");
+var activeoption1 = document.querySelectorAll("[option-active='false']");
+var activeoption2 = document.querySelectorAll("[option-active='true']");
+var customselect = document.querySelectorAll("[custom-select='true']");
+var dropdownlist = document.querySelectorAll("[custom-select='false']");
 
-// for(let j = 0;j < radiobtn.length;j++){
-//     radiobtn[j].addEventListener("click",function(event){
-//         for(let i = 0;i < radiobtn.length;i++){
-//             radiobtn[i].parentElement.parentElement.classList.remove("option-active");
-//         }
-        
-//         // if(radiobtn[j].checked){
-            
-//         //     // radiobtn[j].setAttribute("checked","true");
-//         //     event.currentTarget.parentElement.parentElement.classList.add("option-active");
-//         // }
-//         radiobtn[j].parentElement.parentElement.classList.add("option-active");
-//     })
-// }
+/* Applying background for radio button when selecting */
 
-selectbtn.forEach(function(el){
+activeoption1.forEach(function(el){
     el.addEventListener("click",function(){
-        selectbtn.forEach(function(e){
-            e.classList.remove("option-active");
+        activeoption1.forEach(function(e){
+            e.classList.remove("zln-active");
         })
-        el.classList.add("option-active");
+        el.classList.add("zln-active");
     })  
 })
+
+activeoption2.forEach(function(el){
+    el.addEventListener("click",function(){
+        activeoption2.forEach(function(e){
+            e.classList.remove("zln-active");
+        })
+        el.classList.add("zln-active");
+    })  
+})
+
+/* Applying background for radio button when selecting */
+
+/* For hide and show the dropdown */
+
+customselect.forEach(function(event){
+    event.addEventListener("click",function(){
+        event.classList.toggle("zln-open");
+    })
+})
+
+dropdownlist.forEach(function(dropdown){
+    dropdown.addEventListener("click",function(){
+        customselect.forEach(function(event){
+            event.classList.remove("zln-open");
+        })
+    })
+})
+
+/* For hide and show the dropdown */
